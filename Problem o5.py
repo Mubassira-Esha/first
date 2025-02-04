14ba0658e648b7e610253345fcf774d122dfe626
Mubassira-Esha <mubassiratuljannatesha@gmail.com>
2025-02-05 00:28:03 +0600

    Update Problem o5.py

--- Problem o5.py
+++ Problem o5.py
@@ -1,7 +1,4 @@
 # Problem 5: Calculate the factorial of a number using a for loop.
-# Sample Input (when prompted): 5
-# Expected Output:
-#   Factorial of 5 is 120
 
 n = int(input("Enter a non-negative integer: "))  # e.g., 5
 
