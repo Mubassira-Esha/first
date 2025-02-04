d1091556f9a150cca2df5ea00473dad580e2fe44
Mubassira-Esha <mubassiratuljannatesha@gmail.com>
2025-02-05 00:27:42 +0600

    Update Problem o4.py

--- Problem o4.py
+++ Problem o4.py
@@ -1,7 +1,5 @@
 # Problem 4: Find the second highest number from a set of numbers.
-# Sample Input (when prompted): 10 20 4 45 99 45 20
-# Expected Output:
-#   The second highest number is: 45
+
 
 user_input = input("Enter numbers separated by space: ")  # e.g., "10 20 4 45 99 45 20"
 numbers = [int(x) for x in user_input.split()]
