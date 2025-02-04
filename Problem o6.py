7f81e9e544a2de9b6607ccc40509e6368530a055
Mubassira-Esha <mubassiratuljannatesha@gmail.com>
2025-02-05 00:28:23 +0600

    Update Problem o6.py

--- Problem o6.py
+++ Problem o6.py
@@ -1,8 +1,4 @@
 # Problem 6: Generate Fibonacci series.
-# Sample Input (when prompted): 10
-# Expected Output:
-#   Fibonacci series with 10 terms:
-#   [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
 n_terms = int(input("Enter the number of terms for the Fibonacci series: "))  # e.g., 10
 
